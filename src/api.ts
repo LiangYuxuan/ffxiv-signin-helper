d67cb6ea00e2ab8eec451f38b6a84cb4af3aa0a7
LiangYuxuan <lyx@lyx.es>
2024-10-21 17:38:12 +0800

    feat: update user-agent

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import assert from 'node:assert';
 import got from 'got';
 
-const UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36';
+const UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36';
 
 interface APIReturn {
     code: number;
